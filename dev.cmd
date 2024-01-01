3f6c0ad88ae1a922e2785a9d2bd447ed90c90806
eskopp <skopp.erik@gmail.com>
2024-01-01 22:46:09 +0100

    stuff

--- dev.cmd
+++
@@ -1,6 +0,0 @@
-pip uninstall LiveChessCloud -y
-pip install .
-LiveChessCloud download "https://view.livechesscloud.com/#1eb49a34-ddb6-436a-b1bf-f4fc03c488d1"
-LiveChessCloud export "https://view.livechesscloud.com/#1eb49a34-ddb6-436a-b1bf-f4fc03c488d1"
-LiveChessCloud help
-LiveChessCloud export "https://view.livechesscloud.com/#1eb49a34-ddb6-436a-b1bf-f4fc03c488d1" "output.pgn"
