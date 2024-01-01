ae0b66c0f9ee910de8c4b5ca06306b6adf11c275
eskopp <skopp.erik@gmail.com>
2024-01-01 17:34:09 +0100

    as

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def def_readme():
 
 setuptools.setup(
     name="LiveChessCloud",
-    version="0.0.1",
+    version="0.0.2",
     author="eskopp",
     description="PGN Downloader for LiveChessCloud",
     long_description=def_readme(),
