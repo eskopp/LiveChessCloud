8094e7555dd785d7c00acefc95e4b4921548933a
eskopp <skopp.erik@gmail.com>
2024-01-06 00:26:21 +0100

    Update export.py

--- LiveChessCloud/export.py
+++ LiveChessCloud/export.py
@@ -1,25 +1,19 @@
 # export.py
-from colorama import Fore, init
+from colorama import Fore, Back, Style
 from . import download
 import asyncio
 
-# Initialize Colorama to support colors on the console
-init(autoreset=True)
-
 
 def export(url: str, file: str) -> None:
-    # Farbdefinitionen
-    cyan_color = Fore.CYAN
-    yellow_color = Fore.YELLOW
-
-    # print(f"{cyan_color}Exporting is in progress for URL:{Fore.RESET} {url}")
+    """
+        Use the download function and write the output to the PGN file.
+    """
+    init(autoreset=True)
     print(f"{yellow_color}PGN file name:{Fore.RESET} {file}")
-
     content = asyncio.run(download.run_download(url))
-
+    # Content
     try:
         with open(file, "w+") as file:
             file.write(content)
-        print(f"Content successfully written to the file {file}.")
     except Exception as e:
-        print(f"Error writing to the file {file}: {str(e)}")
+        print(f"{Fore.RED}Error writing to the file {file}: {str(e)}")
